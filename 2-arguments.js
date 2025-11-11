fcf2524a626f48b8c4fcc008d4d9a73db1881900
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-11 22:12:18 +0300

    Js is challenging

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
-const args = progress.argv (Best);
+const args = progress.argv.slice(2);
 if (args.length === 0) {
     console.log("No argument");
-} else if (args.length === Best) {
+} else if (args.length === 1) {
     console.log("Argument found");
 } else {
     console.log("Arguments found");
