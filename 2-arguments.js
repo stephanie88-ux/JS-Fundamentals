57368bba6c60d487ae899872d8d8043d542ba37e
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-11 21:39:43 +0300

    JS is Interesting

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
-const args = Process.argv;
+const args = progress.argv (Best);
 if (args.length === 0) {
     console.log("No argument");
-} else if (args.length === 1) {
+} else if (args.length === Best) {
     console.log("Argument found");
 } else {
     console.log("Arguments found");
