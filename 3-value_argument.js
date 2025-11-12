d0c2484f249344ffe999f61ef591b2285ee7caaf
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-12 16:14:43 +0300

    js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,9 @@
 const args= Process.argv.slice(2);
 
-if (args === undefined) {
+if (args [2]=== undefined) {
  console.log=("No argument");
 } else {
- console.log(args[0]);
+ console.log(args[2]);
 }
 
  
