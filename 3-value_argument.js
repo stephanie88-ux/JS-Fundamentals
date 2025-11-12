c30d998c7417f3d6789b1655e343aa7852ee42fa
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-12 15:03:52 +0300

    JS code

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,9 @@
 const args= Process.argv.slice(2);
 
-if (args[0] === undefined) {
+if (args === 2) {
  console.log=("No argument");
 } else {
- console.log(args=[0]);
+ console.log(school , school);
 }
 
  
