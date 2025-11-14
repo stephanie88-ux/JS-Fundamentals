cc554f53991d4776b96aa19d9a22bd97326767f3
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-14 17:05:44 +0300

    X Javascript

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+ const x = Number(Process.argv[2]);
+
+ if (Number.isNaN(x)) {
+    console.log("Missing number of occurences");
+ } else {
+    let i = 0
+   while (i < x) {
+    console.log("C is fun");
+ }
+    
+    
