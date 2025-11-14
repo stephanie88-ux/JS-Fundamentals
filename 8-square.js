0ddb4287ee3a17fcf44c06d9366eeeeaaaf8470b
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-14 21:38:53 +0300

    squares

--- 8-square.js
+++ 8-square.js
@@ -2,7 +2,7 @@ const size = Number(process.argv[2]);
 
  if (Number.isNaN(size)) {
     console.log("Missing size");
- } else 
+ } else {}
     let i = 0;
     while (i < size) {
      console.log("X".repeat(size));
