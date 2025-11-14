18344dd430b0648bc309ee84125a8911c58d4f85
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-14 16:38:14 +0300

    loops

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 const lines = [ "C is fun", "Python is cool", "JavaScript is amazing"];
  let i = 0;
   while (i < lines.length); {
-    console.log("line i");
+    console.log("C is fun", "JavaSript", "JavaScript is amazing");
 }
    
