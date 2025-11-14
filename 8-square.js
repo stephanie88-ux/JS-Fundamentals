68bc204ed0f6b0ad6740dc8282b9257539fe06b2
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-14 21:24:29 +0300

    squares

--- 8-square.js
+++ 8-square.js
@@ -4,9 +4,11 @@ const size = Number(process.argv[2]);
     console.log("Missing size");
  }
       
-} else {
+ { else } 
     let i = 0;
     while (i < size);
     console.log("X".repeat(size));
     i++;
+ {
+    
  }
