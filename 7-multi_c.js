ebe024d1ab0ce7e7ce55784c2c2af5b10e984663
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-14 18:52:46 +0300

    I Love C

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,8 +3,9 @@
  if (Number.isNaN(x)) {
     console.log("Missing number of occurences");
  } else {
-   while (i < x)  
-    console.log("C is fun");
+  let i = 0;
+while (i < x)  
+    console.log("C is fun"); i++;
  }
      
    
