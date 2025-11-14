5356de8600e723eec7c81db3876dda3e9e178d93
stephaniechepkosgei <stephieem88@gmail.com>
2025-11-14 15:21:49 +0300

    JS integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 const arg = Process.argv[2];
 const num = Number(arg);
 
-if (Number.isNaN(num)) {
+if (Number.isNaN) {
     console.log("Not a number");
 } else {
     console.log("My number " + num);
